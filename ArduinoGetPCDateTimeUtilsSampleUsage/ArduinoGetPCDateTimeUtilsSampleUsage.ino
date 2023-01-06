d61290f38a21090c586e53fb62b62e1da5e771a0
edbecnel <edbecnel@gmail.com>
2023-01-06 09:38:05 +0800

    Updated sample usage sketch to more clearly show how to use with RTCZero board
    Now, the RTCZero-related code is enabled by default and code to simulate using the RTCZero board is commented out.
    Code now more clearly shows how to use the DateAndTimeBytes class in a practical setting.

--- ArduinoGetPCDateTimeUtilsSampleUsage/ArduinoGetPCDateTimeUtilsSampleUsage.ino
+++ ArduinoGetPCDateTimeUtilsSampleUsage/ArduinoGetPCDateTimeUtilsSampleUsage.ino
@@ -1,129 +1,108 @@
-// #include <RTCZero.h>
+#include <RTCZero.h>
 #include <ArduinoGetPCDateTimeUtils.h>
 
 using namespace ArduinoGetPCDateTimeUtils;
 
-byte alarmMonth;
-byte alarmYear;
-byte alarmDay;
-byte alarmHours;
-byte alarmMinutes;
-byte alarmSeconds;
-
-byte currMonth;
-byte currDay;
-byte currYear;
-byte currHours;
-byte currMinutes;
-byte currSeconds;
+// Comment out the line below if testing without the RTCZero board
+RTCZero rtc;
 
 int delayInSecs = 5;
 
-DateAndTimeBytes compileDateAndTimeBytes;
+DateAndTimeBytes compilerDateAndTime;
+DateAndTimeBytes currentDateAndTime;
+DateAndTimeBytes alarmDateAndTime;
 
+// Set the compiler date and time on the RTCZero board
+void SetCompilerDateAndTime()
+{
+  compilerDateAndTime.getCompileDateAndTime();
+  // Set compiler date and time on the RTCZero board...
+  // Note: DateAndTimBytes.year is a byte data type and as such, has a maximum value of 127, so the year value doesn't include the century. I.e., 2022 is represented as 22
+  // Comment out the lines below if testing without the RTCZero board
+  rtc.setDate(compilerDateAndTime.month, compilerDateAndTime.day, compilerDateAndTime.year);
+  rtc.setTime(compilerDateAndTime.hours, compilerDateAndTime.minutes, compilerDateAndTime.seconds);
+}
+
+// Retrieve the current date and time set on the RTCZero board
 void GetCurrentDateAndTime()
 {
-  // currMonth = rtc.getMonth();
-  // currDay = rtc.getDay();
-  // currYear = rtc.getYear();
-  // currHours = rtc.getHours();
-  // currMinutes = rtc.getMinutes();
-  // currSeconds = rtc.getSeconds();
-  currMonth = 12;
-  currDay = 31;
-  currYear = 22;
-  currHours = 23;
-  currMinutes = 13;
-  currSeconds = 5;
+  // Get the date and time that is set on the RTCZero board...
+  currentDateAndTime.month = rtc.getMonth();
+  currentDateAndTime.day = rtc.getDay();
+  currentDateAndTime.year = rtc.getYear();
+  currentDateAndTime.hours = rtc.getHours();
+  currentDateAndTime.minutes = rtc.getMinutes();
+  currentDateAndTime.seconds = rtc.getSeconds();
+  // Replace the above with code below to test without the RTCZero board...
+  // currentDateAndTime.month = 12;
+  // currentDateAndTime.day = 31;
+  // currentDateAndTime.year = 22;
+  // currentDateAndTime.hours = 23;
+  // currentDateAndTime.minutes = 13;
+  // currentDateAndTime.seconds = 5;
 }
 
-// RTCZero rtc;
-void setup() {
-  Serial.begin(115200);
-
-  // rtc.begin();  // nitialize RTC 24H format
-
-  GetCurrentDateAndTime();
-
-  // put your setup code here, to run once:
-  bool success = compileDateAndTimeBytes.getCompileDateAndTime();
-  if (success) {
-    // Set compiler date and time on the RTCZero board...
-    // Note: DateAndTimBytes.year is a byte data type and as such, has a maximum value of 127, so the year value doesn't include the century. I.e., 2022 is represented as 22
-    // rtc.setDate(compileDateAndTimeBytes.month, compileDateAndTimeBytes.day, compileDateAndTimeBytes.year);
-    // rtc.setTime(compileDateAndTimeBytes.hours, compileDateAndTimeBytes.minutes, compileDateAndTimeBytes.seconds);
-    // Verify the date and time was set on the board...
-    // setMonth = rtc.getMonth();
-    // setDay = rtc.getDay();
-    // setYear = rtc.getYear();
-    // setHours = rtc.getHours();
-    // setSMinutes = rtc.getMinutes();
-    // setSeconds = rtc.getSeconds();
-
-    // Set the alarm date and time on the RTCZero board...
-    // alarmMonth = 11;
-    // alarmDay = 3;
-    // alarmYear = 23;
-    // alarmHours = 1;
-    // alarmMinutes = 23;
-    // alarmSeconds = 52;
-    alarmMonth = 12;
-    alarmDay = 31;
-    alarmYear = 22;
-    alarmHours = 23;
-    alarmMinutes = 14;
-    alarmSeconds = 50;
-
-    // rtc.setAlarmDate(alarmDay, alarmMonth, alarmYear);
-    // Set alarm time
-    // rtc.setAlarmTime(alarmHours, alarmMinutes, alarmSeconds);
-  }
+// Retrieve the alarm date and time set on the RTCZero board
+void GetAlarmDateAndTime()
+{
+  // Note: If testing without the RTCZero board, comment out the contents of this function to use the previously set date and time
+  // Get the alarm date and time that is set on the RTCZero board...
+  alarmDateAndTime.month = rtc.getAlarmMonth();
+  alarmDateAndTime.day = rtc.getAlarmDay();
+  alarmDateAndTime.year = rtc.getAlarmYear();
+  alarmDateAndTime.hours = rtc.getAlarmHours();
+  alarmDateAndTime.minutes = rtc.getAlarmMinutes();
+  alarmDateAndTime.seconds = rtc.getAlarmSeconds();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  delay(delayInSecs * 1000);
-
-  // TODO:
-  // Replace hard-coded values with values retrieve from the RTCZero object by commenting out lines with
-  // hard-coded values and uncommenting lines with procedurally retreived values from the rtc object.
-
-  // Get the alarm date and time that was  set on the board...
-  // alarmMonth = rtc.getAlarmMonth();
-  // alarmDay = rtc.getAlarmDay();
-  // alarmYear = rtc.getAlarmYear();
-  // alarmHours = rtc.getAlarmHours();
-  // alarmMinutes = rtc.getAlarmMinutes();
-  // alarmSeconds = rtc.getAlarmSeconds();
-  // Get the current rtc date and time
-  // GetCurrentDateAndTime();
-  
+void GetSimulatedCountdownDateAndTime()
+{
   // Simulated clock ticking up.
   // WARNING: Only works until the current day goes over 28, then resets the current day to 1
-  currSeconds += (byte)delayInSecs;
-  if (currSeconds > 59)
+  currentDateAndTime.seconds += (byte)delayInSecs;
+  if (currentDateAndTime.seconds > 59)
   {
-    currSeconds = 0;
-    currMinutes++;
-    if (currMinutes > 59)
+    currentDateAndTime.seconds = 0;
+    currentDateAndTime.minutes++;
+    if (currentDateAndTime.minutes > 59)
     {
-      currMinutes = 0;
-      if (currHours > 23)
+      currentDateAndTime.minutes = 0;
+      if (currentDateAndTime.hours > 23)
       {
-        currHours = 0;
-        currDay++;
-        if (currDay > 28)
-          currDay = 1;
+        currentDateAndTime.hours = 0;
+        currentDateAndTime.day++;
+        if (currentDateAndTime.day > 28)
+          currentDateAndTime.day = 1;
       }
     }
   }
-   
-  // DateAndTimeBytes currentDateAndTime(currMonth, currDay, currYear, currHours, currMinutes, currSeconds);
-  // DateAndTimeBytes alarmDateAndTime(alarmMonth, alarmDay, alarmYear, alarmHours, alarmMinutes, alarmSeconds);
-  DateAndTimeBytes currentDateAndTime(currMonth, currDay, currYear, currHours, currMinutes, currSeconds);
-  DateAndTimeBytes alarmDateAndTime(alarmMonth, alarmDay, alarmYear, alarmHours, alarmMinutes, alarmSeconds);
- 
+}
+
+// Prints the date and time along with an optional preceding message. The format is: Month/Day/Year Hours:Minutes:Seconds
+void DisplayDateAndTime(char* messagePrefix, DateAndTimeBytes dateAndTime)
+{
+  char buffer[50] = "";
+  if (messagePrefix != NULL && strlen(messagePrefix) > 0)
+  {
+    Serial.print(messagePrefix);
+    Serial.print(": ");
+  }
+  sprintf(buffer, "%02d/", dateAndTime.month);
+  Serial.print(buffer);
+  sprintf(buffer, "%02d/", dateAndTime.day);
+  Serial.print(buffer);
+  sprintf(buffer, "%02d ", dateAndTime.year);
+  Serial.print(buffer);
+  sprintf(buffer, "%02d:", dateAndTime.hours);
+  Serial.print(buffer);
+  sprintf(buffer, "%02d:", dateAndTime.minutes);
+  Serial.print(buffer);
+  sprintf(buffer, "%02d", dateAndTime.seconds);
+  Serial.println(buffer);
+}
+
+void DisplayCountdownTimeToAlarm()
+{
   long daysToAlarm, hoursToAlarm, minutesToAlarm, secondsToAlarm;
   // Get the remaining days, hours, minutes, and seconds to the alarm...
   currentDateAndTime.getDaysHoursMinutesSecondsTo(alarmDateAndTime, daysToAlarm, hoursToAlarm, minutesToAlarm, secondsToAlarm);
@@ -136,12 +115,55 @@ void loop() {
   Serial.print(buffer);
   sprintf(buffer, "Seconds = %02d", secondsToAlarm);
   Serial.println(buffer);
+}
 
-  // TODO: Remove the following when using the rtc object. It's used here only for debugging purposes.
-  // Reset the current date and time back to its initial settings when the alarm is time has been reached (secondsTo remaining is 0 or less).
-  long secondsTo = currentDateAndTime.secondsTo(alarmDateAndTime);
-  if (secondsTo <= 0)
-  {
-    GetCurrentDateAndTime();
-  }
+// Set the alarm date and time on the RTCZero board
+void SetAlarmDateAndTime()
+{
+  // Assign values for alarm date and time
+  alarmDateAndTime.month  = 12;
+  alarmDateAndTime.day = 31;
+  alarmDateAndTime.year = 22;
+  alarmDateAndTime.hours = 23;
+  alarmDateAndTime.minutes = 14;
+  alarmDateAndTime.seconds = 50;
+
+  // Set the alarm date and time on the RTCZero board...
+  // Comment out the lines below if testing without the RTCZero board
+  rtc.setAlarmDate(alarmDateAndTime.day, alarmDateAndTime.month, alarmDateAndTime.year);
+  rtc.setAlarmTime(alarmDateAndTime.hours, alarmDateAndTime.minutes, alarmDateAndTime.seconds);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Comment out the line below if testing without the RTCZero board
+  rtc.begin();  // Initialize RTC 24H format
+
+  delay(delayInSecs * 1000);  // Delay to provide time to clear Serial Monitor for debugging purposes
+
+  SetCompilerDateAndTime();   // Set the compiler date and time as the current time on the RTCZero board
+  DisplayDateAndTime("Compiler date and time", compilerDateAndTime);
+  GetCurrentDateAndTime();    // Get the current date and time on the RTCZero board to confirm it has been set
+  DisplayDateAndTime ("Current date and time", currentDateAndTime);
+  SetAlarmDateAndTime();
+  DisplayDateAndTime ("Alarm date and time set", alarmDateAndTime);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  delay(delayInSecs * 1000);
+
+  // Get the current date and time that is set on the RTCZero board
+  GetCurrentDateAndTime();
+  // If testing without the RTCZero board, replace the above line of code with the following to retrieve a simulated date and time...
+  // GetSimulatedCountdownDateAndTime();
+  DisplayDateAndTime ("Current date and time", currentDateAndTime);
+
+  // Get the alarm date and time that is set on the RTCZero board
+  GetAlarmDateAndTime();
+  DisplayDateAndTime ("Alarm date and time", alarmDateAndTime);
+
+  DisplayCountdownTimeToAlarm();  
 }
